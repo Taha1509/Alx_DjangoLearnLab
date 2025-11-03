6518b856367c806d7f61112221f7527700bbc6a3
unknown <taha1509.ahmed32@gmail.com>
2025-11-03 02:15:17 +0200

    Book.objects.create

--- Introduction_to_Django/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookshelf',  # ← Add this line
+    'bookshelf',  
 ]
 
 MIDDLEWARE = [
