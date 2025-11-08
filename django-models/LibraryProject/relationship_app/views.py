9764353a9120fe15fec370fc6facebffe5b813dd
unknown <taha1509.ahmed32@gmail.com>
2025-11-08 18:28:16 +0200

    2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Book
 from .models import Library
-from django.views.generic import DetailView, ListView
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 
