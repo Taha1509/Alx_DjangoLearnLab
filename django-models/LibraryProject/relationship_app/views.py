d52fc3ca15d66da0ec326eaec17a917cd8380aed
unknown <taha1509.ahmed32@gmail.com>
2025-11-09 02:28:14 +0200

    3

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -22,7 +22,7 @@ class LibraryDetailView(DetailView):
 
 
 class RegisterView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
 
