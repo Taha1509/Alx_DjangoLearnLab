f8200bba4e0b9c91f9da0148da15b0d1f3007fd5
unknown <taha1509.ahmed32@gmail.com>
2025-11-28 03:16:59 +0200

    2

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -11,8 +11,8 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('books', BookListView.as_view()(), name='book-list'),
     path('book/<int:pk>/', BookDetailView.as_view()(), name = 'book-details'),
-    path('book/new/', BookCreateView.as_view(), name='book-create'),
-    path('book/<int:pk>/edit/',BookUpdateView.as_view(), name='book-update'),
-    path('book/<int:pk>/delete/',BookDeleteView.as_view(), name='book-delete'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/update/',BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/',BookDeleteView.as_view(), name='book-delete'),
 
 ]
